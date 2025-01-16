6f0db5b2622758fcc803bb4778564658e7ebe6f7
Catalin Gabrovschi <catalin.gabrovschi@snyk.io>
2025-01-16 15:51:21 +0200

    fix: reintroduce code vulnerabilities

--- src/main.go
+++ src/main.go
@@ -6,10 +6,13 @@ import (
 	"github.com/hashicorp/go-retryablehttp"
 )
 
+var password = "mypassword"
+
 func main() {
 	resp, err := retryablehttp.Get("/path")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(resp)
+	fmt.Println(password)
 }
